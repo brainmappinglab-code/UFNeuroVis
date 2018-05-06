f68fd32e5ec1514314cfe14b2d8b9b5078850af8
hjgauss <repos@opri.it>
2018-05-06 10:17:47 -0400

    adding option to merge slices with different exposure, setting it as default=true

--- LeadLocalization/dependencies/unixDCMtoNIFTI/hjimg_dcm2nii.m
+++ LeadLocalization/dependencies/unixDCMtoNIFTI/hjimg_dcm2nii.m
@@ -1,9 +1,17 @@
-function hjimg_dcm2nii(Patient_DIR, NifTi_DIR)
+function hjimg_dcm2nii(Patient_DIR, NifTi_DIR, is_merge_exp)
     % matlab wrapper to call the DICOM to NifTi converter based on Unix tools
     % It sorts through DICOM images without looking at the DICOMDIR, 
     % reconstructing the info from the DICOM headers present in each image
+    % options:
+    % -is_merge_exp: (default true) merging exposure (some centers may use varying exposure during CT)
+    %
     % Enrico Opri 2018
 
+    if ~exist('is_merge_exp','var')
+        %merging exposure (some centers may use varying exposure during CT)
+        is_merge_exp=true;
+    end
+    
     if ~any(strcmp(Patient_DIR(end),{'\','/'}))
         Patient_DIR = [Patient_DIR,''];
     end
@@ -25,7 +33,11 @@ function hjimg_dcm2nii(Patient_DIR, NifTi_DIR)
     %hjimg__dcmsort = ['"',NEURO_VIS_PATH_UNIX,'dependencies/unixDCMtoNIFTI/hjimg__dcmsort"'];
     %hjimg__convert_tonii = ['"',NEURO_VIS_PATH_UNIX,'dependencies/unixDCMtoNIFTI/hjimg__convert_tonii"'];
     hjimg__dcmsort = [NEURO_VIS_PATH_UNIX,'dependencies/unixDCMtoNIFTI/hjimg__dcmsort'];
-    hjimg__convert_tonii = [NEURO_VIS_PATH_UNIX,'dependencies/unixDCMtoNIFTI/hjimg__convert_tonii'];
+    if is_merge_exp
+        hjimg__convert_tonii = [NEURO_VIS_PATH_UNIX,'dependencies/unixDCMtoNIFTI/hjimg__convert_tonii_mergeexp'];
+    else
+        hjimg__convert_tonii = [NEURO_VIS_PATH_UNIX,'dependencies/unixDCMtoNIFTI/hjimg__convert_tonii'];
+    end
     
     tempfolder1=hfullfile(fileparts(fileparts(NifTi_DIR)),'DICOMSORT/','-unix');
 
