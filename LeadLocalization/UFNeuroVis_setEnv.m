4b78e6f1492dff2512dea6ac8425ff319d8cfc59
hjgauss <repos@opri.it>
2018-05-06 09:54:39 -0400

    fixed setenv

--- LeadLocalization/UFNeuroVis_setEnv.m
+++ LeadLocalization/UFNeuroVis_setEnv.m
@@ -19,7 +19,7 @@ if isempty(getenv('NEURO_VIS_PATH'))
         [~, cwf1]=fileparts(NEURO_VIS_PATH);
         
         %ensure the selected path is correct
-        if strcmp(cwf1,'LeadLocalization')
+        if ~strcmp(cwf1,'LeadLocalization')
             msgbox('Failed to select LeadLocalization folder');
             return
         end
