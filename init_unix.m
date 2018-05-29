9a70fe93a4948b475a0ad80d2e2dc2aed99f2f83
hjgauss <repos@opri.it>
2018-05-29 16:29:19 -0400

    adding initialization script for unix (set execution permissions)

---
+++ init_unix.m
@@ -0,0 +1,5 @@
+%set the correct permission to use on UNIX
+unix('chmod +x LeadLocalization/dependencies/dcm2nii/dcm2niix.glnxa64');
+unix('chmod +x LeadLocalization/dependencies/unixDCMtoNIFTI/hjimg__convert_tonii');
+unix('chmod +x LeadLocalization/dependencies/unixDCMtoNIFTI/hjimg__convert_tonii_mergeexp');
+unix('chmod +x LeadLocalization/dependencies/unixDCMtoNIFTI/hjimg__dcmsort');
