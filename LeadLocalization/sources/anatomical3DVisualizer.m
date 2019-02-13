0226cce6d3e27a6f016b281506a93aaf3dc1951f
Jackson Cagle <jcagle@ufl.edu>
2019-02-13 18:35:26 -0500

    Update new UI for color scale

--- LeadLocalization/sources/anatomical3DVisualizer.m
+++ LeadLocalization/sources/anatomical3DVisualizer.m
@@ -12,7 +12,7 @@ function handles = anatomical3DVisualizer(figureHandler, NifTi)
 %   J. Cagle, 2018
 
 handles.gui = figureHandler; clf(handles.gui);
-set(handles.gui, 'Renderer', 'opengl')
+set(handles.gui, 'Renderer', 'opengl');
 
 handles.Nii = NifTi;
 handles.anatomicalView = axes(handles.gui, 'Units', 'Normalized', 'Position', [0 0 1 1], 'DataAspectRatio',[1 1 1]); 
@@ -20,6 +20,10 @@ hold(handles.anatomicalView,'on');
 handles.anatomicalSlices(1) = generateAnatomicalSlice(handles, handles.Nii.MeshSagittal, 1, handles.Nii.XRange, round(length(handles.Nii.XRange)/2));
 handles.anatomicalSlices(2) = generateAnatomicalSlice(handles, handles.Nii.MeshCoronal, 2, handles.Nii.YRange, round(length(handles.Nii.YRange)/2));
 handles.anatomicalSlices(3) = generateAnatomicalSlice(handles, handles.Nii.MeshAxial, 3, handles.Nii.ZRange, round(length(handles.Nii.ZRange)/2));
+handles.ColorScale = [0 1];
+caxis(handles.anatomicalView, handles.ColorScale * diff(handles.Nii.intensityRange) + handles.Nii.intensityRange(1)); 
+%caxis(handles.anatomicalSlices(2), handles.ColorScale * diff(handles.Nii.intensityRange) + handles.Nii.intensityRange(2)); 
+%caxis(handles.anatomicalSlices(3), handles.ColorScale * diff(handles.Nii.intensityRange) + handles.Nii.intensityRange(3)); 
 
 axis(handles.anatomicalView,[handles.Nii.XRange([1 end]), handles.Nii.YRange([1 end]), handles.Nii.ZRange([1 end])]);
 axis(handles.anatomicalView,'off');
@@ -27,10 +31,35 @@ axis(handles.anatomicalView,'vis3d');
 view(handles.anatomicalView,3);
 colormap(handles.anatomicalView, 'gray');
 
+handles.colorScaleButton = uimenu(handles.gui, 'Label', 'Color Scale', 'Callback', @setColorScale);
+
 set(handles.gui, 'KeyPressFcn', @keyboardEvent)
 
 % Update handles structure
 guidata(handles.gui, handles);
+
+function setColorScale(hObject, eventdata)
+handles = guidata(hObject);
+
+h = largeFigure(0,[1290 200]); clf;
+controller(1) = uicontrol('Style', 'Slider', 'Parent', h, 'Units', 'normalized', 'Position', [0.2 0.7 0.6 0.1], 'Value', handles.ColorScale(1), 'Callback', @updateColorScale);
+controller(2) = uicontrol('Style', 'Slider', 'Parent', h, 'Units', 'normalized', 'Position', [0.2 0.4 0.6 0.1], 'Value', handles.ColorScale(2), 'Callback', @updateColorScale);
+uicontrol('Style','pushbutton','String','Done','Callback','uiresume(gcbf)', 'Units', 'normalized', 'Position', [0.45 0.1 0.1 0.1]);
+uiwait(gcf);
+if (controller(1).Value > controller(2).Value)
+    controller(1).Value = controller(2).Value;
+end
+handles.ColorScale = [controller(1).Value controller(2).Value];
+caxis(handles.anatomicalView, handles.ColorScale * diff(handles.Nii.intensityRange) + handles.Nii.intensityRange(1)); 
+%caxis(handles.anatomicalSlices(2), handles.ColorScale * diff(handles.Nii.intensityRange) + handles.Nii.intensityRange(2)); 
+%caxis(handles.anatomicalSlices(3), handles.ColorScale * diff(handles.Nii.intensityRange) + handles.Nii.intensityRange(3)); 
+guidata(hObject, handles);
+close(h);
+
+function updateColorScale(hObject, eventdata)
+handles = guidata(hObject.Parent);
+guidata(hObject.Parent, handles);
+
 
 % --- Generate Anatomical Slices for 3D View.
 function surface = generateAnatomicalSlice( handles, Mesh, FixDim, DimRange, DimIndex )
